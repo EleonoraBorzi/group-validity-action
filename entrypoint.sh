b9b7276221c8644638ce41f68870c3d89e9163d0
hengque <43048353+hengque@users.noreply.github.com>
2021-04-05 17:50:03 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
-python3 check_group_validity.py "$1" "$2" "$3" "$4" "$5" "$6"
+python3 /app/check_group_validity.py "$1" "$2" "$3" "$4" "$5" "$6"
 
 ls / -a
 echo "-"
 ls /app/ -a
 echo "-"
 ls ../ -a
+echo "-"
+ls ../../ -a
+echo "-"
+ls ../../../ -a
