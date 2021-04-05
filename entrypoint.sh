0b3a3d8217ceb69b96fcdc3aedd45bb437343492
EleonoraBorzis <47255376+EleonoraBorzis@users.noreply.github.com>
2021-04-05 19:16:06 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 json=$(python3 /app/check_group_validity.py "$1" "$2" "$3" "$4" "$5" "$6")
+echo "Helloooooooooooooooooooooooooo"
 echo $json
-print(json)
-
-
 
 json="${json//'%'/'%25'}"
 json="${json//$'\n'/'%0A'}"
