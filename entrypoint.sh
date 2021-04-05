9b330e8d66ed8e599b3768e43e7e06b7ed78a256
EleonoraBorzis <47255376+EleonoraBorzis@users.noreply.github.com>
2021-04-05 19:18:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,9 @@ echo $json
 json="${json//'%'/'%25'}"
 json="${json//$'\n'/'%0A'}"
 json="${json//$'\r'/'%0D'}"
-apt-get install jq
 
-jq '.student_submission' json 
+jq '.student_submission' $json 
 
-if(($json >> jq -r '.student_submission')=="true"); then 
+if(($json | jq -r '.student_submission')=="true"); then 
   exit 1
 fi
