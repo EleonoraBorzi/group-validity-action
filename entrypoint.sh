09fc229b2f401d21fc770d5f5b1a622e4ae2b520
EleonoraBorzis <47255376+EleonoraBorzis@users.noreply.github.com>
2021-04-05 19:03:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,8 @@
-python3 /app/check_group_validity.py "$1" "$2" "$3" "$4" "$5" "$6"
+json=$(python3 /app/check_group_validity.py "$1" "$2" "$3" "$4" "$5" "$6")
+json="${json//'%'/'%25'}"
+json="${json//$'\n'/'%0A'}"
+json="${json//$'\r'/'%0D'}"
+sudo apt-get install jq
+if((json >> jq -r '.student_submission')=="true"); then 
+  exit 1
+fi
