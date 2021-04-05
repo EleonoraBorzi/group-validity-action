b9ab47f68c3598b40f17d96e8a23b3c8019e8b6b
hengque <43048353+hengque@users.noreply.github.com>
2021-04-05 17:10:33 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 python3 check_group_validity.py "$1" "$2" "$3" "$4" "$5" "$6"
+
+ls -a
+echo "-"
 ls .. -a
-
+echo "-"
 ls ./head/ -a
-
+echo "-"
 ls ./head/contributions/ -a
-
+echo "-"
 ls ./head/contributions/henke-borzi/ -a
+echo "-"
+ls ./base/ -a
+echo "-"
